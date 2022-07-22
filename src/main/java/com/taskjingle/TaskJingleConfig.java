5c2fe491c1462ed4941961edaba82fdee6dd834a
max <maxreumer@gmail.com>
2022-07-22 17:39:54 +0200

    Added custom mp3

--- src/main/java/com/taskjingle/TaskJingleConfig.java
+++ src/main/java/com/taskjingle/TaskJingleConfig.java
@@ -4,7 +4,6 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.Range;
-
 @ConfigGroup("taskjingle")
 public interface TaskJingleConfig extends Config
 {
@@ -20,5 +19,26 @@ public interface TaskJingleConfig extends Config
 	default int volume()
 	{
 		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "customjingle",
+			name = "Custom Jingle",
+			description = "Toggles whether a custom jingle is used",
+			position = 2
+	)
+	default boolean customjingle()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "custompath",
+			name = "Custom path",
+			description = "Specify file path or URL(must contain http:// or https://) to custom MP3",
+			position = 3
+	)
+	default String custompath()
+	{
+		return "";
 	}
 }
--- src/main/java/com/taskjingle/TaskJinglePlugin.java
+++ src/main/java/com/taskjingle/TaskJinglePlugin.java
@@ -3,6 +3,7 @@ package com.taskjingle;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
@@ -14,6 +15,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.Text;
 
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.*;
@@ -32,17 +34,34 @@ public class TaskJinglePlugin extends Plugin
 	private TaskJingleConfig config;
 	private static final Pattern CHAT_COMPLETE_MESSAGE = Pattern.compile("You've completed (?:at least )?(?<tasks>[\\d,]+) (?:Wilderness )?tasks?(?: and received \\d+ points, giving you a total of (?<points>[\\d,]+)| and reached the maximum amount of Slayer points \\((?<points2>[\\d,]+)\\))?");
 	private MP3Player trackPlayer = new MP3Player(getClass().getClassLoader().getResource("task-jingle.mp3"));
-
+	private MP3Player customtrack = new MP3Player(getClass().getClassLoader().getResource("task-jingle.mp3"));
 
 
 	@Subscribe
-	public void onChatMessage(ChatMessage event) {
+	public void onChatMessage(ChatMessage event) throws Exception {
 		trackPlayer.setVolume(config.volume());
 		String chatMsg = Text.removeTags(event.getMessage()); //remove color and linebreaks
 		if (chatMsg.startsWith("You've completed") && (chatMsg.contains("Slayer master") || chatMsg.contains("Slayer Master"))) {
 			Matcher mComplete = CHAT_COMPLETE_MESSAGE.matcher(chatMsg);
 
 			if (mComplete.find()) {
+				if (config.customjingle()){
+					if (config.custompath().endsWith(".mp3")||config.custompath().endsWith(".MP3") )
+					{
+					if (config.custompath().startsWith("http://") || config.custompath().startsWith("https://") ){
+						customtrack = new MP3Player(new URL(config.custompath()));
+					}
+
+					{
+					customtrack = new MP3Player(new File(config.custompath()));
+					}
+					}
+					else{
+						customtrack = new MP3Player(getClass().getClassLoader().getResource("task-jingle.mp3"));
+					}
+					customtrack.play();
+				}
+				else
 				trackPlayer.play();
 			}
 		}
